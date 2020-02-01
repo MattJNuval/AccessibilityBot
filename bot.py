0a7d6f3b6361b97f29765826d095416228ba487b
Mathew Nuval <nuval.mathewj@gmail.com>
2020-02-01 12:49:47 -0800

    Added on_member_join() functionality

--- bot.py
+++ bot.py
@@ -3,18 +3,49 @@ import discord
 
 TOKEN = ''
 
+
 client = discord.Client()
+
+
+global isMemberJoined
+isMemberJoined = False
+
+
+@client.event
+async def on_member_join(member):
+	print("A user joined")
+	global isMemberJoined
+	isMemberJoined = True
+
 
 @client.event
 async def on_message(message):
+
+
+
 	userMassage = ''
 	if message.author == client.user:
 		return
 
-	username = str(message.author)
 	
+	username = str(message.author)
+
+	joinMessage = ''
+	
+	global isMemberJoined
+	if isMemberJoined == True:
+		joinMessage = 'A user name: ' + username[0:int(len(username)-5)]  + ' joined the server!'
+		isMemberJoined = False
+		await message.channel.send(joinMessage, tts = True, delete_after = 5.0)
+		return 
+
+
+
 	userMassage += username[0:int(len(username)-5)] + ' said: ' + message.content
-	await message.channel.send(userMassage, tts = True, delete_after = 5.0);
+	await message.channel.send(userMassage, tts = True, delete_after = 5.0)
+
+
+
 
 @client.event
 async def on_ready():
