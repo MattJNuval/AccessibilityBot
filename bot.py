9be3da33a087439afb8adae9159c9235282ac78d
Mathew Nuval <nuval.mathewj@gmail.com>
2020-01-31 22:41:03 -0800

    added bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # Work with Python 3.6
 import discord
 
-TOKEN = 'NjczMDQ5NzEyMDAxMDI0MDQw.XjUYcg.Jen461xXPetBbY_HkTs_QGCpMxk'
+TOKEN = 'NjczMDQ5NzEyMDAxMDI0MDQw.XjUdQA.NE0k1JSKS7LdqQdDmhpxG9F6Pzs'
 
 client = discord.Client()
 
